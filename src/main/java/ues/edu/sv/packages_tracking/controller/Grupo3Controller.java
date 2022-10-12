483aac386e1db21fa80b5ca56d45aa5ba5fc18ba
sergioperez <sergioperez@localhost>
2022-10-12 16:14:34 -0600

    Vista 3, corrida 1

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo3Controller.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/Grupo3Controller.java
@@ -1,5 +1,12 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class Grupo3Controller {
-    
+     @GetMapping("/")
+    public String inicio(Model model){
+        return "example";
+    }
 }
