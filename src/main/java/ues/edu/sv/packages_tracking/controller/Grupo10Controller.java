2cf9cb882e6041b575bc0184c13413da3a334a26
Josué Hernández <joheca1998@outlook.com>
2022-10-12 15:50:31 -0600

    Inicializando controlador de página login

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
@@ -1,5 +1,15 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping(value = "/main")
 public class Grupo10Controller {
-    
+    @GetMapping(value = "/home")
+    public String loginView() {
+        return "login";
+    }
+
 }
