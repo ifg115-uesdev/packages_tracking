b2538447e2569b45adb64df7acc2eeed7006e8f8
Josué Hernández <joheca1998@outlook.com>
2022-10-12 18:28:05 -0600

    Arreglando controlador para la vista login

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
@@ -2,14 +2,11 @@ package ues.edu.sv.packages_tracking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping(value = "/main")
 public class Grupo10Controller {
-    @GetMapping(value = "/home")
+    @RequestMapping(value = "/login")
     public String loginView() {
         return "login";
     }
-
 }
