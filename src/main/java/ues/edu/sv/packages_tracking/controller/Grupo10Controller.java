a05c8285a8db737e0caf296179430e15e2297090
KevzRamz <115606095+KevzRamz@users.noreply.github.com>
2022-10-16 13:07:27 -0600

    Update Grupo10Controller.java

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
@@ -1,5 +1,12 @@
 package ues.edu.sv.packages_tracking.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class Grupo10Controller {
+   @RequestMapping(value = "/login")
+    public String loginView() {
+        return "login";
+    }
     
 }
