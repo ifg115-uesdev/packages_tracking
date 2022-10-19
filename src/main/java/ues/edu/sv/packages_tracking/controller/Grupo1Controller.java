4960e2e9d61a79f5cc62882f61e87b3290e24fd2
Christian Avalos <christianavalos311997@gmail.com>
2022-10-19 17:32:07 -0600

    modificacion se creó el mapeo para tabla de transporte y se creo la estructura del proyecto para fase 2

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo1Controller.java
+++
@@ -1,6 +0,0 @@
-package ues.edu.sv.packages_tracking.controller;
-
-
-public class Grupo1Controller {
-    
-}
---
+++ src/main/java/ues/edu/sv/packages_tracking/serviceImpl/AgenciaServiceImpl.java
@@ -0,0 +1,10 @@
+package ues.edu.sv.packages_tracking.serviceImpl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AgenciaServiceImpl {
+    
+}
