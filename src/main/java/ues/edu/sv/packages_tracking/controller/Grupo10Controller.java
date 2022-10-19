ad685cfa40a410df604090b11f9b45da80165990
Josué Hernández <joheca1998@outlook.com>
2022-10-19 11:10:57 -0600

    corrigiendo nombres de las vistas y arreglando controlador

--- src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/Grupo10Controller.java
@@ -1,16 +1,12 @@
 package ues.edu.sv.packages_tracking.controller;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/login")
 public class Grupo10Controller {
-
-
-    @GetMapping
+    @RequestMapping(value = "/login")
     public String loginView() {
-        return "vista9";
+        return "login";
     }
-    
 }
