e8089e817504b3c1762bce4b99cfccfd0626393a
FerCortez28 <50848000+FerCortez28@users.noreply.github.com>
2022-10-22 22:17:14 -0600

    Prueba commit
    Primer avance

--- src/main/java/ues/edu/sv/packages_tracking/controller/AgenciaController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/AgenciaController.java
@@ -1,10 +1,18 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ues.edu.sv.packages_tracking.service.AgenciaService;
+
 @Controller
-@RequestMapping(value = "/agencia")
 public class AgenciaController {
-    
+    @RequestMapping(value = "/PruebaS")
+    private String Save(){
+        return "crear_editar_agencias.html";
+    }
+
+    @Autowired
+    private AgenciaService agenciaService;
 }
