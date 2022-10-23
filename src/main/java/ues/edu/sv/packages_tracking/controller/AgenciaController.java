a4bc3b36339bbe3e0810cb98d3e9e47ebc66948a
Daniel Servellon <sr17033@ues.edu.sv>
2022-10-22 22:11:43 -0600

    Devuelve la vista
    Devuelve la pantalla Crear_Editar_aganecia 22-10-22 10:09

--- src/main/java/ues/edu/sv/packages_tracking/controller/AgenciaController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/AgenciaController.java
@@ -1,10 +1,23 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ues.edu.sv.packages_tracking.service.AgenciaService;
 
 @Controller
 @RequestMapping(value = "/agencia")
 public class AgenciaController {
-    
+    @Autowired
+    private AgenciaService agenciaService;
+
+    @GetMapping(value="edit_agencia")
+    public String editar(){
+        return"crear_editar_agencias.html";
+
+    }
+
+
 }
--- src/main/java/ues/edu/sv/packages_tracking/serviceImpl/AgenciaServiceImpl.java
+++ src/main/java/ues/edu/sv/packages_tracking/serviceImpl/AgenciaServiceImpl.java
@@ -1,10 +1,37 @@
 package ues.edu.sv.packages_tracking.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ues.edu.sv.packages_tracking.entities.Agency;
+import ues.edu.sv.packages_tracking.repository.AgenciaRepository;
+import ues.edu.sv.packages_tracking.service.AgenciaService;
+
 @Service
 @Transactional
-public class AgenciaServiceImpl {
+public class AgenciaServiceImpl implements AgenciaService{
+    @Autowired
+    private AgenciaRepository agenciaRepository;
+    
+    @Override
+    public List<Agency> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void guardarAgencia(Agency agency) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Agency obtenerPorId(Integer idAgencia) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
