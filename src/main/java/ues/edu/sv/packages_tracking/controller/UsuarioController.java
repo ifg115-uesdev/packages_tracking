bfcd59f8dbc1f0832b9565221e84fc3a9b02ba32
ar21106 <ar21106@ues.edu.sv>
2022-10-22 22:12:14 -0500

    Prueba commit
    abrir vista desde el controlador

--- src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
@@ -1,10 +1,21 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ues.edu.sv.packages_tracking.service.UsuarioService;
+
 @Controller
-@RequestMapping(value = "/usuario")
+@RequestMapping(value = "/usuarios")
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping(value="/editar_usuario")
+    public String prueba(){
+        return "crear_editar_usuarios.html";
+    } 
+
 }
--- src/main/java/ues/edu/sv/packages_tracking/serviceImpl/UsuarioServiceImpl.java
+++ src/main/java/ues/edu/sv/packages_tracking/serviceImpl/UsuarioServiceImpl.java
@@ -1,10 +1,37 @@
 package ues.edu.sv.packages_tracking.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ues.edu.sv.packages_tracking.entities.User;
+import ues.edu.sv.packages_tracking.repository.UsuarioRepository;
+import ues.edu.sv.packages_tracking.service.UsuarioService;
+
 @Service
 @Transactional
-public class UsuarioServiceImpl {
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void guardarAgencia(User user) {
+        
+    }
+
+    @Override
+    public User obtenerPorId(Integer idUsuario) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
