8247f6483520a73dd16a9c9600969e1615354620
Samuel Perez <pp21017@ues.edu.sv>
2022-10-23 19:09:44 -0600

    Practicando en el controlador usuario

--- src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
@@ -1,10 +1,25 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ues.edu.sv.packages_tracking.service.UsuarioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @Controller
 @RequestMapping(value = "/usuario")
-public class UsuarioController {
+public class UsuarioController  {
+    
+    @Autowired
+     private UsuarioService servicio;
+
+     @GetMapping(value="/allUsers")
+     public String editarUsuario() {
+         return "listado_usuarios.html";
+     }
+     
     
 }
