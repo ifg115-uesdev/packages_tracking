5a1d7e94f330e6595b195284c44dbe6cb135ceb9
Karla Roman <kmene2021@gmail.com>
2022-10-23 21:20:08 -0600

    cambios

--- src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/UsuarioController.java
@@ -2,9 +2,13 @@ package ues.edu.sv.packages_tracking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
-    
+    @GetMapping(value="/listado_usuarios")
+    public String mostrarlistausuarios(){
+    return "crear_editar_usuarios.html";
+        
+    }
 }
