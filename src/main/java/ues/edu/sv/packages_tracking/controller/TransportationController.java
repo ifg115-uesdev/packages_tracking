84a97236ff3a0a37f1517dabb8eb34ee2d07fd67
Christian Avalos <christianavalos311997@gmail.com>
2022-11-06 16:07:10 -0600

    modificaciones en controlador de transporte

--- src/main/java/ues/edu/sv/packages_tracking/controller/TransportationController.java
+++ src/main/java/ues/edu/sv/packages_tracking/controller/TransportationController.java
@@ -1,13 +1,23 @@
 package ues.edu.sv.packages_tracking.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import ues.edu.sv.packages_tracking.entities.StatePackage;
+import ues.edu.sv.packages_tracking.entities.Package;
 import ues.edu.sv.packages_tracking.entities.Transportation;
+import ues.edu.sv.packages_tracking.service.PackageService;
+import ues.edu.sv.packages_tracking.service.StatePackageService;
 import ues.edu.sv.packages_tracking.service.TransportationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,9 +29,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequestMapping(value="/transportation")
 public class TransportationController {
+
+    Integer idT = 0;
     
     @Autowired
     TransportationService service;
+
+    @Autowired
+    PackageService packageService;
+
+    @Autowired
+    StatePackageService statePackageService;
 
     @GetMapping(value="/all")
     public String transportationList(Model model) {
@@ -65,4 +83,29 @@ public class TransportationController {
         return "redirect:/transportation/update/"+id;
     }
     
+    @GetMapping(value="/packages")
+    public String getViewTransportationPackage() {
+        return "consultar_transporte";
+    }
+
+    @GetMapping(value="/packages-transportation")
+    public String getPackagesByTransportationId(@RequestParam("transportationId") Integer id ,Model model) {
+        List<Package> list = packageService.findByTransportationId(id);
+        List<StatePackage> stateList = statePackageService.getAllStates().stream().filter(s -> s.getStatePackageId() > 5).collect(Collectors.toList());
+
+        Map<String,Object> map= new HashMap<>();
+        map.put("paquetes", list);
+        map.put("estados", stateList);
+        model.addAllAttributes(map);
+        this.idT = id;
+        return "consultar_transporte";
+    }
+
+    @PostMapping(value="/update-state-package")
+    public String updateTransportation(@ModelAttribute("paquetes")ArrayList<Package> paquetes, @RequestParam("stateP") Integer stateP) {
+        
+        System.out.println(stateP);
+        System.out.println(this.idT);
+        return "redirect:/transportation/packages-transportation?transportationId=1";
+    }
 }
